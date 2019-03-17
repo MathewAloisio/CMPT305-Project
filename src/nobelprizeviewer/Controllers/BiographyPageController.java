05f2e47d7008f0ced1f671765d26ea7cf93649e3
Tam Le <philosocode@gmail.com>
2019-03-17 16:02:40 -0600

    Add biography v1

--- src/nobelprizeviewer/Controllers/BiographyPageController.java
+++ src/nobelprizeviewer/Controllers/BiographyPageController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nobelprizeviewer;
+package nobelprizeviewer.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
